ed3ad40690119cbc41f4cdc489318c05c4999ecd
LeoTan2004 <3509836544@qq.com>
2024-05-26 11:25:01 +0800

    C语言重构
    由于Python在嵌入式方面的跨平台存在明显的缺陷,我们尝试使用C语言重新构建

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    // 读取图像
+    Mat image = imread("../resource/test-set/img/0005.jpg", IMREAD_COLOR);
+
+    // 判断是否读取成功
+    if (image.empty())
+    {
+        cout << "Could not open or find the image" << endl;
+        return -1;
+    }
+
+    // 显示图像
+    imshow("Image", image);
+
+    // 等待按键
+    waitKey(0);
+
+    return 0;
+}
